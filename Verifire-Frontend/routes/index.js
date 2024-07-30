1a0d73646fd068eff0a79ccbef69b41fbd002d86
noa-okutomi <okutomi.noa@dmap.jp>
2024-07-30 18:31:56 +0900

    [fix]VP要求ボタンを押下後のフローを追加

--- Verifire-Frontend/routes/index.js
+++ Verifire-Frontend/routes/index.js
@@ -9,3 +9,68 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// GET /initiate：VP要求ボタンを押下後
+router.get('/initiate', function(retiq, res, next){
+
+  const date = {
+    type: "vp_token",
+    presentation_definition: {
+      id: "32f54163-7166-48f1-93d8-ff217bdb0653",
+      input_descriptors:[
+        {
+            id: "eu.europa.ec.eudi.pid.1",
+            name: "EUDI PID",
+            purpose: "We need to verify your identity",
+            format: {
+                mso_mdoc: {
+                    alg: ["ES256", "ES384", "ES512", "EdDSA"]
+                }
+            },
+            constraints:{
+                fields:[
+                    {
+                        path:["$['eu.europa.ec.eudi.pid.1']['family_name']"],
+                        intent_to_retain: true
+                    }
+                ]
+            }
+        }
+        ]
+    },
+    nonce: "9b1271e7-f85b-43cd-8a66-2bfdaee1eeb2",
+    wallet_response_redirect_uri_template: "http://localhost:3000/get-wallet-code?response_code={RESPONSE_CODE}"
+  }
+
+  axios.post('https://verifier-backend.eudiw.dev/ui/presentations', date)
+  .then(response => {
+    console.log('responseUrl:',response.data);
+    // 後続Get wallet response実行のためにpresentation_idを取得
+    presentationId = response.data.presentation_id;
+    console.log('presentationId:',presentationId);
+
+    // request_uriを取得
+    const requestUri = response.data.request_uri;
+    console.log('requestUri',requestUri);
+
+    // request_uriをエンコード
+    const encodedURI = encodeURIComponent(requestUri);
+    console.log('encodedURI:',encodedURI);
+
+    // eudi-openid4vp~のURLを生成
+    const url = "eudi-openid4vp://verifier-backend.eudiw.dev?client_id=verifier-backend.eudiw.dev&request_uri=" + encodedURI;
+    console.log('url:',url);
+
+    // verifiable画面のQRコードを生成する
+    QRcode.toDataURL(url, function(err, qrCodeDataURL){ // QRコードを生成後、コールバック関数を実行
+      if(err){
+        console.log('error:',err);
+      }
+      console.log('qrCodeDataURL:',qrCodeDataURL);
+    // リンクとQRコードを渡す
+      res.render('verifiable', {qrCodeDataURL: qrCodeDataURL, url: url});
+    });
+  })
+  .catch(error => {
+    console.log('error:',error);
+  });
+});
