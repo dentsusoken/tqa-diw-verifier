3d28505d4f80cac4b98c7bc7cb055588799839fa
noa-okutomi <okutomi.noa@dmap.jp>
2024-08-15 14:09:51 +0900

    [fix]必要なライブラリのインストール

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 
@@ -12,6 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(session({
+  secret:'secret',
+  resave:false,
+  saveUninitialized:false
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
